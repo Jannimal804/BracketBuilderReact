8da17f56d639c2670e929f15cdb34a940e236869
David Byrd <dbyrd@depparts.com>
2019-04-16 19:09:20 -0400

    map added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import logo from "./logo.svg";
 import "./App.css";
 import NavTabs from "./NavTabs";
+import Map from "./components/Map"
 
 class App extends Component {
   render() {
     return (
       <Router>
         <NavTabs />
+          <Route exact path="/gyms" component={Map} />
       </Router>
     );
   }
