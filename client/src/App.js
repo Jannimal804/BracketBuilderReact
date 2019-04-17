8e5f92d978c658edec45475fe123b196a9fb5154
sharitacumm <emandraine@yahoo.com>
2019-04-17 19:43:45 -0400

    working calendar

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
+// import { BrowserRouter as Router} from "react-router-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import NavTabs from "./NavTabs";
+import Login from "./login/renderer";
+import Calendar from "./calendar/calrenderer";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <NavTabs />
+        <Route path = "/login" component = {Login} />
+        <Route path = "/calendar" component = {Calendar} />
       </Router>
     );
   }
