b3ce3ba9dd28e690e4997f7724a4ed506ff9a9b9
sharitacumm <emandraine@yahoo.com>
2019-04-23 21:10:48 -0400

    Complete Login Skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import NavTabs from "./NavTabs";
 import Map from "./components/Map"
 import CompetitorCard from "./components/CompetitorCard";
 import Profile from "./components/Pages/Profile"
+import Login from "./login/renderer";
+import Calendar from "./calendar/calrenderer";
 
 
 import Form from "./components/Form"
@@ -20,6 +22,9 @@ function App() {
         <Route exact path="/gyms" component={Map} />
         <Route exact path="/competitors" component={CompetitorCard} />
         <Route exact path="/profile" component={Profile} />
+        <Route exact path="/login" component={Login} />
+        <Route path = "/calendar" component = {Calendar} />
+        
       </div>
     </Router>
   );
