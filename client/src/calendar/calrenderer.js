9ca4c82b346f06a124db297040d6302bf78b956e
sharitacumm <emandraine@yahoo.com>
2019-04-25 20:15:22 -0400

    working database

--- client/src/calendar/calrenderer.js
+++ client/src/calendar/calrenderer.js
@@ -2,15 +2,14 @@ import React from "react";
 import reactDom from "react-dom";
 
 
-export default class Calendar extends React.Component{
+export default class Calendar extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {};
     }
     render() {
-        return <iframe name="cw_frame" width="100%" height="1200" src="https://www.calendarwiz.com/calendars/calendar.php?crd=bracketbuilder&cid[]=all">Your browser does not support inline frames or is currently configured not to display inline frames.</iframe>
-
+        return <iframe name="cw_frame" width="100%" height="1200" src="https://www.calendarwiz.com/calendars/calendar.php?crd=bracketbuilder&nolog=1&cid[]=all">Your browser does not support inline frames or is currently configured not to display inline frames.</iframe>
 
     }
 }
